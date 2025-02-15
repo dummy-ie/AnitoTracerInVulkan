c96c032df4593488fcc9b3b4c56f8387502ae67a
FinalSpeedZ <85392584+FinalSpeedZ@users.noreply.github.com>
2025-02-15 17:41:06 +0800

    Fixed Imgui initialization

--- src/From-GDGRAP2/RTConfig.h
+++ src/From-GDGRAP2/RTConfig.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <filesystem>
 #include <string>
 
 struct ApplicationConfig
@@ -6,7 +7,11 @@ struct ApplicationConfig
 	static constexpr float ASPECT_RATIO = 16.0f / 10.0f;
 	static const int APP_WINDOW_WIDTH = 1920;
 	static constexpr int APP_WINDOW_HEIGHT = static_cast<int>(APP_WINDOW_WIDTH / ASPECT_RATIO);
-	static const inline std::string DEFAULT_UI_LAYOUT_PATH = "X:/GithubProjects/RayTracingInVulkan/src/imgui_default_layout.ini";
+
+	static const inline std::string DEFAULT_UI_LAYOUT_PATH = ([] {
+		std::filesystem::path slnDir = SOLUTION_DIR; 
+		return (slnDir / "src/imgui_default_layout.ini").string();
+		})();
 };
 
 struct RTConfig_Debug
--- src/UserInterface.cpp
+++ src/UserInterface.cpp
@@ -82,7 +82,7 @@ UserInterface::UserInterface(
 
 	auto& io = ImGui::GetIO();
 
-	io.IniFilename = "imgui.ini";
+	io.IniFilename = "../../../src/imgui.ini";
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
 	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
